13332264f5a382434f6f77ae43d2621107cd0416
AndresHense <andreshense@gmail.com>
2022-02-17 14:14:40 -0300

    add testimony model, and add get request

---
+++ routes/testimonies.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var router = express.Router()
+const db = require('../models/index')
+const Sequelize = require('sequelize')
+/* GET users listing. */
+router.get('/:id/public', async (req, res, next) => {
+  const testimonies = await db.Testimony.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+  if (testimonies) {
+    res.json(testimonies)
+  } else {
+    res.status(404)
+  }
+})
+
+module.exports = router

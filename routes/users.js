bef406a32b9f4917213843332e019d314cc7cf7e
AndresHense <andreshense@gmail.com>
2022-02-18 11:52:33 -0300

    move validation to a middleware

--- routes/users.js
+++ routes/users.js
@@ -8,30 +8,28 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post(
-  '/auth/login',
-  body('email').isEmail(),
-  body('password').isLength({ min: 5 }),
-  function (req, res, next) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array(), ok: false });
-    }
+const userValidation = () => {
+  return [(body('email').isEmail(), body('password').isLength({ min: 5 }))];
+};
 
-    const { email, password } = req.body;
-    const user = await db.User.find({
-      where: {
-        email: email,
-      },
-    });
-
-    //Do I need to encrypt or decrypt one of the passwords here?
-    if (user && user.password == password) {
-      res.json(user);
-    } else {
-      res.status(401).json({ ok: false });
-    }
+router.post('/auth/login', userValidation(), async function (req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array(), ok: false });
   }
-);
+  const { email, password } = req.body;
+  const user = await db.User.find({
+    where: {
+      email: email,
+    },
+  });
+
+  //Do I need to encrypt or decrypt one of the passwords here?
+  if (user && user.password === password) {
+    res.json(user);
+  } else {
+    res.status(401).json({ ok: false });
+  }
+});
 
 module.exports = router;

a35425f8d83a3a9e4b38da73bc5f57704f2682c0
AndresHense <andreshense@gmail.com>
2022-02-18 11:15:14 -0300

    fixes in routes/users.js
    - add semicolons
    - remove async in router.post
    - remove comments
    - add db module

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.post(
   '/auth/login',
   body('email').isEmail(),
   body('password').isLength({ min: 5 }),
-  async function (req, res, next) {
+  function (req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array(), ok: false });

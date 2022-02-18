c390c780414376b4d69128666b2b4e61f89b7966
AndresHense <andreshense@gmail.com>
2022-02-18 11:06:09 -0300

    delete process.env.DB_DIALECT

--- config/config.js
+++ config/config.js
@@ -7,14 +7,14 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
   },
   production: {
     username: process.env.DB_USER,

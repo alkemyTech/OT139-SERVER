215d0e896233a97bf42baa58ad18f28612e7f9de
AndresHense <andreshense@gmail.com>
2022-02-22 15:39:37 -0300

    prepare files to merge main

--- config/config.js
+++ config/config.js
@@ -7,20 +7,20 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
 };

3677f7686d09a6d62a3f751771cfd68f6efeaed1
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2022-02-22 22:37:08 -0300

    OT139-41 Crear migracion y modelo de categorias

---
+++ models/categories.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class categories extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  categories.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      deletedAd: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "categories",
+    }
+  );
+  return categories;
+};

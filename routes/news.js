963ddfe57efdd01611fac490aea4061f34b3c1b4
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2022-02-23 20:41:41 -0300

    OT139-48-Endpoint Detalle Novedad

---
+++ routes/news.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const db = require("models/index");
+
+router.get("/news/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const newsById = await db.Entries.findByPk({ id: id });
+    res.json(newsById);
+  } catch (err) {
+    res
+      .status(404)
+      .json({ Error: `Novedad con Id ${id} no encontrada :` + err });
+  }
+});
+
+module.exports = router;

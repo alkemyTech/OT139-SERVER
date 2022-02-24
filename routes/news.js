20a595cab49e7b084100256fb51924fd93b9bb00
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2022-02-23 21:07:57 -0300

    1-agrego ruta a app.js 2-correccion de endpoint  news.js (/:id)

--- routes/news.js
+++ routes/news.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("models/index");
 
-router.get("/news/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const newsById = await db.Entries.findByPk({ id: id });

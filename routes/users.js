b9f4e07033767661d06d2adab8c9bd06d07cbd5c
Rivadeneira Lichardi Lucas <lrlichardi@hotmail.com>
2022-02-25 19:54:35 -0300

    create delete route and controller user for unsuscribe

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { unSuscriberUser } = require ('../controllers/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  console.log(req.body)
 });
+
+router.delete('/:id' , unSuscriberUser)
 
 module.exports = router;

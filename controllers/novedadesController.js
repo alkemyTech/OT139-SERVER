331a3b7482410628d6a56984351324743793045c
tamiisoledad <tamiisoledad4@gmail.com>
2022-02-28 14:09:25 -0300

    se actualiza el endpoint de actualizaciones

--- controllers/novedadesController.js
+++ controllers/novedadesController.js
@@ -2,6 +2,28 @@ const db = require('../models');
 
 module.exports = {
     updated: (req, res) => {
-        
+        db.New.update({
+            name: req.body.name,
+            content: req.body.content,
+            image: req.file.filename,
+            categoryId: req.body.categoryId,
+            type: req.body.type
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(result => {
+            let resultado = {
+                meta: {
+                    status: 201,
+                    total: result.length,
+                    url: '/news/update/' + req.params.id
+                },
+                data: result
+            }
+            res.json(resultado)
+        })
+        .catch(error => console.log(error));
     }
 }

fa9f6301dddf524617385e4e8e95f808958458e9
Hugo Venega <hugo.venega.00@gmail.com>
2022-03-02 14:51:26 -0300

    Correciones segun la PR

--- controllers/senderEmailController.js
+++ controllers/senderEmailController.js
@@ -1,12 +1,13 @@
 const sgMail = require('@sendgrid/mail');
+const Acepted = require('../constants/httpCodes');
 
 async function senderEmail(recieverEmail) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  const sendStatus = '';
+  const sendStatus = 'Success';
   const msg = {
     to: recieverEmail,
-    from: process.env.SENDGRID_VERIFIED_SENDER, // Change to your verified sender
+    from: process.env.SENDGRID_VERIFIED_SENDER,
     subject: 'Sending with SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
@@ -15,14 +16,12 @@ async function senderEmail(recieverEmail) {
   await sgMail
     .send(msg)
     .then((response) => {
-      sendStatus = response[0].statusCode;
+      if(response[0].statusCode === Acepted) {
+        return sendStatus;
+      }
     })
     .catch((error) => {
-      sendStatus = error.response.body.errors[0];
+      return error;
     });
 
-    if (sendStatus !== 202) {
-      return sendStatus;
-    } else 
-    {return "SUCCESS"}
 }

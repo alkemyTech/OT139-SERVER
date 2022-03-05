df4205d9e18028ff183fb185552ab2d43d322ada
AndresHense <andreshense@gmail.com>
2022-03-05 17:50:20 -0300

    add http codes and error handling

---
+++ constants/httpCodes.js
@@ -0,0 +1,4 @@
+export const HTTP_CODES = {
+  ACCEPTED: 202,
+  INTERNAL_SERVER_ERROR: 500,
+};
--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,15 +1,19 @@
+const { HTTP_CODES } = require('../constants/httpCodes');
 const db = require('../models');
 
 const getAllContacts = async (req, res, next) => {
   try {
     const contacts = await db.User.findAll();
-    res.status(200).json({
+    res.status(HTTP_CODES.ACCEPTED).json({
       ok: true,
       msg: 'Succesful request',
       result: contacts,
     });
   } catch (error) {
-    next(error);
+    res
+      .status(HTTP_CODES.INTERNAL_SERVER_ERROR)
+      .json({ ok: false, msg: 'internal server error', error });
+    console.log(error);
   }
 };
 

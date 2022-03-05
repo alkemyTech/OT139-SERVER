8227695f69dba2be8f0b80ea737c489636defe79
Rivadeneira Lichardi Lucas <lrlichardi@hotmail.com>
2022-03-05 16:11:39 -0300

    update constans http as object

--- constants/httpCodes.js
+++ constants/httpCodes.js
@@ -1,2 +1,4 @@
-export const Ok = 200;
-export const BadRequest = 400;
+export const HTTP_CODES = {
+    OK: 200,
+    BAD_REQUEST: 400
+  };
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-import { Ok, BadRequest } from '../constants/httpCodes';
+import { OK , BAD_REQUEST } from '../constants/httpCodes';
 
 exports.deleteUser = async (req, res) => {
   try {
@@ -7,9 +7,9 @@ exports.deleteUser = async (req, res) => {
       where: { 
       id: req.params.id
     }});
-    res.status(Ok).send('Fue Dado de Baja exitosamente! Hasta Pronto!');
+    res.status(OK).send('Fue Dado de Baja exitosamente! Hasta Pronto!');
   } catch (errors) {
-    res.status(BadRequest).send({msg:'Ocurrio un error al tratar de dar de baja al usuario'});
+    res.status(BAD_REQUEST).send({msg:'Ocurrio un error al tratar de dar de baja al usuario'});
     console.error(errors.message);
   }
 };

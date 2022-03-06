099c2848731d5141b90d4f12c01c0bed863798f3
Yonpalac <yon.palac@gmail.com>
2022-03-06 14:56:04 +0100

    arregle comentarios del request

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,14 +1,15 @@
-const db = require('../models');
 const { validationResult } = require('express-validator');
+import { OK , BAD_REQUEST } from '../constants/httpCodes';
+const db = require('../models');
 
-const add = async (req, res) => {
+exports.add = async (req, res) => {
     const { name, email } = req.body;
     const errors = validationResult(req);
     
     if (!errors.isEmpty()) {
       const errorsObj = errors.mapped();
       
-      return res.status(422).json({
+      return res.status(BAD_REQUEST).json({
         ok: false,
         msg: "Error validating data",
         result: null,
@@ -22,20 +23,25 @@ const add = async (req, res) => {
           email
       })
       .then(newContact => {
-          res.status(201).json({
+          res.status(OK).json({
               ok: true,
               msg: "Success creating new contact",
               result: newContact
           })
       })
+      .catch(error => {
+        res.status(BAD_REQUEST).json({
+          ok: false,
+          msg: "Error creating new contact",
+          result: error
+        })
+      })
 
     } catch (errors) {
-      return res.status(400).json({
+      return res.status(BAD_REQUEST).json({
         ok: false,
         msg: 'Error creating contact.',
         error: errors,
       })
     }
 }
-
-module.exports = { add };

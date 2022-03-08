8e01a1da26f923f54108b91a1efc6276d55377e0
Facundo_Loto <55114127+facundoloto@users.noreply.github.com>
2022-03-08 16:10:33 -0300

    Update DataBase.js

--- dataBase/DataBase.js
+++ dataBase/DataBase.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD, {
-  host:'localhost',
+  host:DB_HOST,
   dialect:'mysql'
 });
 

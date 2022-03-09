3115c28e71fccfa48ee7007ce4b6352b2763dc25
AndresHense <andreshense@gmail.com>
2022-03-09 13:36:18 -0300

    fix conflict

--- constants/httpCodes.js
+++ constants/httpCodes.js
@@ -1,11 +1,8 @@
 const HTTP_CODES = {
   OK: 200,
   BAD_REQUEST: 400,
-<<<<<<< HEAD
   UNAUTHORIZED: 401,
   INTERNAL_SERVER_ERROR: 500,
-=======
->>>>>>> 5ba6ed45b4eb09f3ffe3ab2db2bb1e6709d8c265
 };
 
 module.exports = HTTP_CODES;

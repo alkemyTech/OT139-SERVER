7b4686ce54d130479721c8d924d0b4c0a86ca093
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-08 22:12:34 -0300

    Creada la logica para el endpoint DELETE /news/:id

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,10 +1,38 @@
 const db = require('../models');
-const HTTP_CODES = require('../constants/httpCodes');
+const { OK, BAD_REQUEST, NOT_FOUND } = require('../constants/httpCodes');
 
 async function deleteNews(req, res) {
-  return res.status(200).json({
-    ok: true,
-  });
+  const { id } = req.params;
+
+  if (Number(id) < 0 || isNaN(id)) {
+    return res.status(BAD_REQUEST).json({
+      error: 'Invalid news id',
+    });
+  }
+
+  try {
+    const existsNews = await db.Entries.findOne({ where: { id } });
+    if (!existsNews) {
+      return res.status(NOT_FOUND).json({
+        error: 'News not found',
+      });
+    }
+
+    const deleted = await db.Entries.destroy({ where: { id } });
+    if (!deleted) {
+      return res.status(BAD_REQUEST).json({
+        error: 'News not deleted',
+      });
+    }
+
+    return res.status(OK).json({
+      ok: true,
+    });
+  } catch (err) {
+    return res.status(BAD_REQUEST).json({
+      error: 'News not deleted',
+    });
+  }
 }
 
 module.exports = {

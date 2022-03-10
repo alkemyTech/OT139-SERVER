cfb23e09dbeb47305dc1d860ddd53a1ed69b445b
Yonpalac <yon.palac@gmail.com>
2022-03-10 12:51:38 +0100

    sume middleware en la carpeta que viene por main

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { add } = require('../controllers/contactController');
-const validation = require('../middlewares/contact');
+const validation = require('../middleware/contact');
 const router = express.Router();
 
 router.post('/', validation, add);

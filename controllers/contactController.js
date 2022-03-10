2df6b9fa3875778bae51062e000e39174bef736a
Rivadeneira Lichardi Lucas <lrlichardi@hotmail.com>
2022-03-10 16:01:33 -0300

    update contactController

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,13 +1,18 @@
+const { BAD_REQUEST } = require('../constants/httpCodes');
 const senderEmail = require ('./senderEmailController');
 
 exports.senderEmailContact = async (req, res) => {
     const text = 'Gracias por contactarnos y querer formar parte de la SOMOS MAS';
     const subject = 'Gracias por su contacto';
     const email = req.body.email;
-    const textHtml = `<strong>${text}</strong>`
-    console.log(textHtml)
+    const textHtml = `<strong>${text}</strong>`;
     
-    // try{
-    //     senderEmail(email , subject , text , textHtml)
-    // }
+    try{
+        await senderEmail(email , subject , text , textHtml);
+    }catch(error){
+            res.status(BAD_REQUEST).send({
+                msg:'Error al enviar el correo'
+            });
+            console.log(error);
+    }
 }

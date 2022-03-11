5deb9596b4e0df03bc516ff209212ae3374eb431
ema <lotofacundo6@gmail.com>
2022-03-10 22:23:12 -0300

    OT139-45-endpoint-listado-novedades-cambios en codigo

--- controllers/NewController.js
+++ controllers/NewController.js
@@ -1,14 +1,13 @@
 const db = require("../models");
 
-exports.news = async(req, res) => {
+exports.news = async(req , res) => {
     try {
         const news = await db.Entries.findAll({
             attributes: ['name','imageUrl','createdAt'],
             where: { categoryID:"news" }
         });
         res.status(200).send(news)
-    }catch(err){
-        console.log(err)
+    } catch(err) {
         res.status(400).send('try again,the server could some problem in this moment');
     };
 };

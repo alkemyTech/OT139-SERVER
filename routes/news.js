bf9dc0e60db45e311d95969ccbd19356d714c5b9
Facundo_Loto <55114127+facundoloto@users.noreply.github.com>
2022-03-10 22:30:31 -0300

    Update news.js

--- routes/news.js
+++ routes/news.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { news } = require('../controllers/NewController');
+const { news } = require('../controllers/newController');
 
 router.get('/', news);
 

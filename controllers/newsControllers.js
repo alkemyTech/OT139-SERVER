a6456ab6b7f93673a403dd1670cab853ec828391
ema <lotofacundo6@gmail.com>
2022-03-11 13:07:44 -0300

    OT139-45-SERVER-endpoint-listado-novedades-cambios codigo

--- controllers/newsControllers.js
+++ controllers/newsControllers.js
@@ -1,9 +1,20 @@
 const db = require('../models');
 const HTTP_CODES = require('../constants/httpCodes');
 
+const news = async (req, res) => {
+    try {
+        const news = await db.Entries.findAll({
+            attributes: ['name','imageUrl','createdAt'],
+            where: { categoryID:"news" }
+        });
+        res.status(HTTP_CODES.OK).send(news);
+    } catch(err) {
+        res.status(HTTP_CODES.NOT_FOUND).send('try again,the server could some problem in this moment');
+    };
+};
+
 const getNewsById = async (req, res) => {
   const { id } = req.params;
-
   try {
     const newsById = await db.Entries.findByPk({ id });
     res.json(newsById);
@@ -16,4 +27,5 @@ const getNewsById = async (req, res) => {
 
 module.exports = {
   getNewsById,
+  news
 };

601fe4af7c73ad969b6e01eadfa37329abf299c8
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-14 16:57:17 -0300

    Creado el seeder para Organizaciones

---
+++ seeders/20220314194941-create-demo-organization.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Organizations', [{
+      name: 'Somos Mas',
+      image:
+      'https://drive.google.com/file/d/1-j70Zmn2B1-0T_67JHJbNLKkI9sACMNi/view?usp=sharing',
+      phone: '1160112988',
+      address: 'Barrio La Cava',
+      welcomeText:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie interdum rutrum. Nulla luctus est eget feugiat condimentum.',
+      createdAt: new Date,
+      updatedAt: new Date,
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Organizations', null, {});
+  }
+};

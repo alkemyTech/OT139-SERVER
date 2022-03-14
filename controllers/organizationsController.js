efcfb8da03c387c8c332d504a4e66350aada1d3f
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-14 17:08:18 -0300

    Se movió la logica del endpoint GET /organizations/1/public a un controlador

---
+++ controllers/organizationsController.js
@@ -0,0 +1,23 @@
+function getOrganization(req, res) {
+  res.json({
+    name: 'Somos Mas',
+    image:
+      'https://drive.google.com/file/d/1-j70Zmn2B1-0T_67JHJbNLKkI9sACMNi/view?usp=sharing',
+    phone: '1160112988',
+    address: 'Barrio La Cava',
+    welcomeText:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie interdum rutrum. Nulla luctus est eget feugiat condimentum.',
+    social: {
+      facebook: 'https://www.facebook.com/corpsomosmas',
+      twitter: 'https://twitter.com/somosmas',
+      vimeo: 'https://vimeo.com/somosmas',
+      linkedin: 'https://www.linkedin.com/company/somosmas/',
+      flirk: 'https://www.flickr.com/photos/corporacionsomosmas/albums',
+      youTube: 'https://www.youtube.com/user/corposomosmas',
+    },
+  });
+}
+
+module.exports = {
+  getOrganization,
+}
--- routes/organization.js
+++ routes/organization.js
@@ -1,24 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { getOrganization } = require('../controllers/organizationsController');
 
-router.get('/1/public', (req, res, next) => {
-  res.json({
-    name: 'Somos Mas',
-    image:
-      'https://drive.google.com/file/d/1-j70Zmn2B1-0T_67JHJbNLKkI9sACMNi/view?usp=sharing',
-    phone: '1160112988',
-    address: 'Barrio La Cava',
-    welcomeText:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie interdum rutrum. Nulla luctus est eget feugiat condimentum.',
-    social: {
-      facebook: 'https://www.facebook.com/corpsomosmas',
-      twitter: 'https://twitter.com/somosmas',
-      vimeo: 'https://vimeo.com/somosmas',
-      linkedin: 'https://www.linkedin.com/company/somosmas/',
-      flirk: 'https://www.flickr.com/photos/corporacionsomosmas/albums',
-      youTube: 'https://www.youtube.com/user/corposomosmas',
-    },
-  });
-});
+router.get('/1/public', getOrganization);
 
 module.exports = router;

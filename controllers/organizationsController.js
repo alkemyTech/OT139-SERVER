b3afa12e0e01f87f1681146444d63093e65f1280
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-03-14 19:13:07 -0300

    Creado el endpoint PUT /organizations/:id

--- controllers/organizationsController.js
+++ controllers/organizationsController.js
@@ -1,5 +1,5 @@
 const db = require('../models');
-const { OK, NOT_FOUND } = require('../constants/httpCodes');
+const { OK, BAD_REQUEST, NOT_FOUND } = require('../constants/httpCodes');
 
 async function getFirstOrganization(req, res) {
   const organizationId = 1;
@@ -7,13 +7,54 @@ async function getFirstOrganization(req, res) {
   try {
     const organization = await db.Organizations.findByPk(organizationId);
     return res.status(OK).json(organization);
-  } catch(err) {
+  } catch (err) {
     return res.status(NOT_FOUND).json({
-      error: 'Organization not found'
-    })
+      error: 'Organization not found',
+    });
+  }
+}
+
+async function editOrganization(req, res) {
+  const { id } = req.params;
+  const organization = req.body;
+
+  try {
+    if (Number(id) < 0 || isNaN(id)) {
+      return res.status(BAD_REQUEST).json({
+        error: 'Invalid organization id',
+      });
+    }
+
+    const existsOrganization = await db.Organizations.findByPk(id);
+
+    if (!existsOrganization) {
+      return res.status(NOT_FOUND).json({
+        error: 'Organization not found',
+      });
+    }
+
+    const updated = await db.Organizations.update({
+      ...organization,
+      updatedAt: Date.now(),
+    },{ where: { id } });
+
+    if (!updated) {
+      return res.status(BAD_REQUEST).json({
+        error: 'Organization not updated',
+      });
+    }
+
+    return res.status(OK).json({
+      ok: true,
+    });
+  } catch (err) {
+    return res.status(BAD_REQUEST).json({
+      error: 'Organization not updated',
+    });
   }
 }
 
 module.exports = {
   getFirstOrganization,
-}
+  editOrganization,
+};
--- routes/organization.js
+++ routes/organization.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getFirstOrganization } = require('../controllers/organizationsController');
+const {
+  getFirstOrganization,
+  editOrganization,
+} = require('../controllers/organizationsController');
 
 router.get('/1/public', getFirstOrganization);
+router.put('/:id', editOrganization);
 
 module.exports = router;

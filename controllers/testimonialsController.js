7b9ce1aaa46be831df33d7a5df0bb60d02ab962a
Yonpalac <yon.palac@gmail.com>
2022-03-17 00:34:07 +0100

    Cambio then y catch por await

--- controllers/testimonialsController.js
+++ controllers/testimonialsController.js
@@ -1,37 +1,45 @@
 const db = require('../models');
-const { OK, BAD_REQUEST } = require('../constants/httpCodes');
+const {
+  OK,
+  BAD_REQUEST,
+  INTERNAL_SERVER_ERROR,
+} = require('../constants/httpCodes');
 
 exports.testimonialsUpdate = async (req, res) => {
-    const { name, image, content } = req.body;
+  const { name, image, content } = req.body;
 
-    const testimonial = await db.Testimonials.findOne({ where: { id: req.params.id } });
+  try {
+    const testimonial = await db.Testimonials.findOne({
+      where: { id: req.params.id },
+    });
 
     if (!testimonial) {
       return res.status(BAD_REQUEST).json({
         ok: false,
         msg: 'El testimonio no fue encontrado.',
-      })
-    };
+      });
+    }
 
-    await db.Testimonials.update({
+    const testimonialUpdate = await db.Testimonials.update(
+      {
         name,
         image,
-        content
-    }, { 
-        where: { id: req.params.id } 
-    })
-    .then((result) => res.status(OK).json({
-        ok: true,
-        msg: 'El testimonio fue editado con exito.',
-        result: { ...result }
-    }))
-    .catch((error) => {
-        res.status(BAD_REQUEST).json({
-        ok: false,
-        msg: 'El testimonio no puede ser editado',
-        error: error
-        })
-    })
-
-    return null
-}
+        content,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    res.status(OK).json({
+      ok: true,
+      msg: 'El testimonio fue editado con exito.',
+      result: { ...testimonialUpdate },
+    });
+  } catch (errors) {
+    return res.status(INTERNAL_SERVER_ERROR).json({
+      ok: false,
+      msg: 'Error creating contact.',
+      error: errors,
+    });
+  }
+};

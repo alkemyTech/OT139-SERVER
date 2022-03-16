81f53149f5897c53a9d5af89435cc77f3e52aac2
Yonpalac <yon.palac@gmail.com>
2022-03-16 13:02:33 +0100

    Agrego endpoint para verificar que los usuarios solo sean accesibles por administradores

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
+const { isAdmin } = require('../middlewares/verify');
 const { userValidation } = require('../middleware/userValidation');
 const { authUser, deleteUser, signUp, getAll } = require('../controllers/usersController');
 
 /* GET users listing. */
-router.get('/', getAll);
+router.get('/', isAdmin, getAll);
 router.post('/auth/login', userValidation(), authUser);
 router.post('/auth/register',
 [

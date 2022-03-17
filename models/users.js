14f79f06c180d407858d7a4b84ad246234479717
AndresHense <andreshense@gmail.com>
2022-03-17 15:39:19 -0300

    modify activities entries and users models

--- models/users.js
+++ models/users.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
     /**
@@ -10,23 +8,25 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Users.belongsTo(models.Role, {as: 'role'});
+      Users.belongsTo(models.Role, { as: 'role' });
     }
-  };
-  Users.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    image: DataTypes.STRING,
-    password: DataTypes.STRING,
-    roleId: DataTypes.INTEGER,
-    deletedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'users', //this users is beacuse in mysql the table is users and not Users
-    paranoid: true, 
-    timestamps: true
   }
-  )
+  Users.init(
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      image: DataTypes.STRING,
+      password: DataTypes.STRING,
+      roleId: DataTypes.INTEGER,
+      deletedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: 'Users',
+      paranoid: true,
+      timestamps: true,
+    }
+  );
   return Users;
 };

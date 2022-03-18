a52344660f6b7e1d23894ceb8828493ce86b900f
Andres Hense <andreshense@gmail.com>
2022-03-18 18:20:29 -0300

    modify activities entries and users models (#51)
    * modify activities entries and users models
    
    * change users to Users in controllers/ and change categories to Categories

--- migrations/20220223013019-create-categories.js
+++ migrations/20220223013019-create-categories.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("categories", {
+    await queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("categories");
+    await queryInterface.dropTable('Categories');
   },
 };

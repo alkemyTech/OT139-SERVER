27c8339c777f7a3604f24f29fb2016e8b01f2007
Yonatan <76592537+YonPalac1@users.noreply.github.com>
2022-03-18 12:07:55 -0300

    Ot139 72 endpoint para almacenar contactos (#23)
    * endpoint de contacto
    
    * Add middleware validation contact
    
    * arregle comentarios del request
    
    * Agregué endpoint para crear contactos y middleware de validaciones
    
    * sume middleware en la carpeta que viene por main
    
    * Arreglado nombre de middleware, y saco then y catch de controlador
    
    * Cambio nombre de middleware

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,5 +1,45 @@
-const { HTTP_CODES } = require('../constants/httpCodes');
+const { validationResult } = require('express-validator');
+const {
+  OK,
+  BAD_REQUEST,
+  INTERNAL_SERVER_ERROR,
+  HTTP_CODES,
+} = require('../constants/httpCodes');
 const db = require('../models');
+
+const addContact = async (req, res) => {
+  const { name, email, phone, message } = req.body;
+  const errorsValidationResult = validationResult(req);
+
+  if (!errorsValidationResult.isEmpty()) {
+    return res.status(BAD_REQUEST).json({
+      ok: false,
+      msg: 'Error validating data',
+      result: null,
+      errorsMapped: errors.mapped(),
+    });
+  }
+
+  try {
+    const newContact = await db.Contacts.create({
+      name,
+      email,
+      phone,
+      message,
+    });
+    res.status(OK).json({
+      ok: true,
+      msg: 'Success creating new contact',
+      result: newContact,
+    });
+  } catch (errors) {
+    return res.status(INTERNAL_SERVER_ERROR).json({
+      ok: false,
+      msg: 'Error creating contact.',
+      error: errors,
+    });
+  }
+};
 
 const getAllContacts = async (req, res, next) => {
   try {
@@ -16,4 +56,4 @@ const getAllContacts = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllContacts };
+module.exports = { getAllContacts, addContact };

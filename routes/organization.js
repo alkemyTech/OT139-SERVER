804def012f0c74ff4ef03e673c18fb5b2818680d
Eric Varela Avalos <45970652+evarelaavalos@users.noreply.github.com>
2022-03-23 19:30:56 -0300

    OT139-50 Screen Edición Organización (admin) (Backend) (#45)
    * Creada la migracion y el modelo de Organizaciones
    
    * Creado el seeder para Organizaciones
    
    * Se movió la logica del endpoint GET /organizations/1/public a un controlador
    
    * La organizacion ahora se obtiene de la base de datos
    
    * Creado el endpoint PUT /organizations/:id
    
    * Ajustando la logica del endpoint GET /organizations/1/public
    
    * El id de organizations ya no es autoincremental
    
    * Se cambio la propiedad image del seeder de organizations

--- routes/organization.js
+++ routes/organization.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getOrganization,
+  editOrganization,
+} = require('../controllers/organizationsController');
 
-router.get('/1/public', (req, res, next) => {
-  res.json({
-    name: 'Somos Mas',
-    image: '/images/LOGO-SOMOS-MAS.png',
-    phone: '1160112988',
-    address: 'Barrio La Cava',
-    welcomeText:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie interdum rutrum. Nulla luctus est eget feugiat condimentum.',
-    social: {
-      facebook: 'https://www.facebook.com/corpsomosmas',
-      twitter: 'https://twitter.com/somosmas',
-      vimeo: 'https://vimeo.com/somosmas',
-      linkedin: 'https://www.linkedin.com/company/somosmas/',
-      flirk: 'https://www.flickr.com/photos/corporacionsomosmas/albums',
-      youTube: 'https://www.youtube.com/user/corposomosmas',
-    },
-  });
-});
+router.get('/:id/public', getOrganization);
+router.put('/:id', editOrganization);
 
 module.exports = router;

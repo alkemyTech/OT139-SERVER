2196777c5e0905aee4ba2b03d03e235941b1f268
tamiisoledad <tamiisoledad4@gmail.com>
2022-03-24 23:11:14 -0300

    endpoint de eliminacion de miembros

--- routes/members.js
+++ routes/members.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getMembers,
   createMember,
+  deleteMember
 } = require('../controllers/membersController');
 
 router.get('/', getMembers);
 router.post('/', createMember);
+router.delete('/:id', deleteMember);
 
 module.exports = router;

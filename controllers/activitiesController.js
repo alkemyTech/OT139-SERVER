a34c7adc7a195a8e555c45348e9deec21ca0c3de
Fran <palacios.francisco@outlook.com>
2022-03-28 19:45:55 -0300

    POST activity - fix PUT activity

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -3,6 +3,7 @@ const HTTP_CODES = require('./../constants/httpCodes');
 
 const getAllActivities = async (req, res) => {
   try {
+    console.log('getAllActivities');
     const activities = await db.Activities.findAll();
     res.status(HTTP_CODES.OK).json(activities);
   } catch (error) {
@@ -10,11 +11,34 @@ const getAllActivities = async (req, res) => {
   }
 };
 
+const createActivity = async (req, res) => {
+  const { name, content } = req.body;
+  if (!name || !content) {
+    return res.status(HTTP_CODES.BAD_REQUEST).json({
+      msg: 'Faltan datos para crear la actividad',
+    });
+  }
+  try {
+    const activity = await db.Activities.create({ name, content });
+    res.status(HTTP_CODES.CREATED).json(activity);
+  } catch (error) {
+    res.status(HTTP_CODES.INTERNAL_SERVER_ERROR).json(error);
+  }
+};
+
 const updatedActivity = async (req, res) => {
   const { id } = req.params;
+  const { name, content } = req.body;
 
   try {
-    const activity = await db.Activity.update({ id }, req.body);
+    const activity = await db.Activities.update(
+      { name, content },
+      {
+        where: {
+          id,
+        },
+      }
+    );
     res.json(activity);
   } catch (error) {
     res.status(HTTP_CODES.BAD_REQUEST).json({
@@ -26,5 +50,6 @@ const updatedActivity = async (req, res) => {
 
 module.exports = {
   updatedActivity,
+  createActivity,
   getAllActivities,
 };
--- routes/activities.js
+++ routes/activities.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
   updatedActivity,
+  createActivity,
   getAllActivities,
 } = require('../controllers/activitiesController');
 
 router.get('/', getAllActivities);
+router.post('/', createActivity);
 router.put('/:id', updatedActivity);
 
 module.exports = router;

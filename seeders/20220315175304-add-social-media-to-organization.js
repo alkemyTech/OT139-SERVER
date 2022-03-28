c5c87225a6b641afa762abf8519e73f3c8e9ecce
AndresHense <andreshense@gmail.com>
2022-03-28 15:50:36 -0300

    add social media to organizations

--- seeders/20220315175304-add-social-media-to-organization.js
+++ seeders/20220315175304-add-social-media-to-organization.js
@@ -2,24 +2,32 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkUpdate('Organizations', {
-      facebook: 'https://www.facebook.com/corpsomosmas',
-      twitter: 'https://twitter.com/somosmas',
-      vimeo: 'https://vimeo.com/somosmas',
-      linkedin: 'https://www.linkedin.com/company/somosmas/',
-      flickr: 'https://www.flickr.com/photos/corporacionsomosmas/albums',
-      youtube: 'https://www.youtube.com/user/corposomosmas',
-    }, { id: 1 });
+    await queryInterface.bulkUpdate(
+      'Organizations',
+      {
+        facebook: 'https://www.facebook.com/corpsomosmas',
+        twitter: 'https://twitter.com/somosmas',
+        vimeo: 'https://vimeo.com/somosmas',
+        linkedin: 'https://www.linkedin.com/company/somosmas/',
+        flickr: 'https://www.flickr.com/photos/corporacionsomosmas/albums',
+        youtube: 'https://www.youtube.com/user/corposomosmas',
+      },
+      { id: 1 }
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkUpdate('Organizations', {
-      facebook: null,
-      twitter: null,
-      vimeo: null,
-      linkedin: null,
-      flickr: null,
-      youtube: null,
-    }, { id: 1 });
-  }
+    await queryInterface.bulkUpdate(
+      'Organizations',
+      {
+        facebook: null,
+        twitter: null,
+        vimeo: null,
+        linkedin: null,
+        flickr: null,
+        youtube: null,
+      },
+      { id: 1 }
+    );
+  },
 };

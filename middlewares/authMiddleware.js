ed7865de3e115f01873f7612b6e75db57915a761
Yonpalac <yon.palac@gmail.com>
2022-03-29 14:57:18 -0300

    Saco sanitize token funcion de middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const { verifyJsonWebToken, sanitizeToken } = require('../helpers/jwt');
 const { UNAUTHORIZED, FORBIDDEN } = require('../constants/httpCodes');
 
 function verifyUser(req, res, next) {
-  const token = sanitizeToken(req.headers.authorization);
+  const token = req.headers.authorization;
 
   if (!token) {
     return res.status(FORBIDDEN).json({

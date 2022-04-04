3451ba9ce14fc2d7c455b8a57052282941b15b29
Rivadeneira Lichardi Lucas <lrlichardi@hotmail.com>
2022-04-04 15:11:20 -0300

    fix describe and httpCodes

--- test/authentication.js
+++ test/authentication.js
@@ -1,7 +1,7 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 const expect = require('chai').expect;
-
+const { OK , UNAUTHORIZED, FORBIDDEN } = require('../constants/httpCodes');
 // DOCUMENTATION
 
 // Test for authentication is created
@@ -11,31 +11,36 @@ const expect = require('chai').expect;
 chai.use(chaiHttp);
 
 const url = 'http://localhost:3000';
-let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJsdWNhcyIsImxhc3ROYW1lIjoiUml2YWRlbmVpcmEiLCJlbWFpbCI6ImxybGljaGFyZGlAaG90bWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCQ5cEsuekdLcjFNMGJKNXo5U3hxcVJla0NDSVIxdHBLVmlkQU9FQnJMRjFLd0NoSlZnalJzMiIsInJvbGVJZCI6MiwiaWF0IjoxNjQ4NzY3MzA5LCJleHAiOjE2NDg4NTM3MDl9.EOXM4vxoRUGVgtMHV2fzIVMWHOkw3NKxeH9GOotYk3c";
+let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJsdWNhcyIsImxhc3ROYW1lIjoiUml2YWRlbmVpcmEiLCJlbWFpbCI6ImxybGljaGFyZGlAaG90bWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCQ5cEsuekdLcjFNMGJKNXo5U3hxcVJla0NDSVIxdHBLVmlkQU9FQnJMRjFLd0NoSlZnalJzMiIsInJvbGVJZCI6MiwiaWF0IjoxNjQ4NzY3MzA5LCJleHAiOjE2NDg4NTM3MDl9.EOXM4vxoRUGVgtMHV2fzIVMWHOkw3NKxeH9GOotYk3c';
 
- describe('Get data user specific', () => {
-    it('Should respond with json containing data user specific', (done) => {
-      chai.request(url)
-      .get('/auth/me')
-      .set({'Authorization': token})
-      .end((err , res) => { 
-        console.log(res.body)
-        expect(res).to.have.status(200);
-        done()
-      })
+describe(
+  'Api /authentication',
+  () => {
+    describe('Get data user specific', () => {
+      it('Should respond with json containing data user specific', (done) => {
+        chai
+          .request(url)
+          .get('/auth/me')
+          .set({ Authorization: token })
+          .end((err, res) => {
+            expect(res).to.have.status(OK);
+            done();
+          });
+      });
     });
-  });
-
-  describe('Error at get data user specific for token', () => {
-    it('Should respond with json containing error for token', (done) => {
-      chai.request(url)
-      .get('/auth/me')
-      .set({'Authorization': ''})
-      .end((err , res) => { 
-        expect(res).to.have.status(403);
-        done()
-      })
+  },
+  () => {
+    describe('Error at get data user specific for token', () => {
+      it('Should respond with json containing error for token', (done) => {
+        chai
+          .request(url)
+          .get('/auth/me')
+          .set({ Authorization: '' })
+          .end((err, res) => {
+            expect(err).to.have.status(FORBIDDEN);
+            done();
+          });
+      });
     });
-  });
-  
- 
+  }
+);

f3d0f53c7bb55889b4010c72978cc55495af3cb2
Rivadeneira Lichardi Lucas <lrlichardi@hotmail.com>
2022-04-05 19:05:27 -0300

    update test authentication

--- test/authentication.js
+++ test/authentication.js
@@ -1,5 +1,5 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const expect = require('chai').expect;
 const { OK , UNAUTHORIZED, FORBIDDEN } = require('../constants/httpCodes');
 // DOCUMENTATION
@@ -11,36 +11,41 @@ const { OK , UNAUTHORIZED, FORBIDDEN } = require('../constants/httpCodes');
 chai.use(chaiHttp);
 
 const url = 'http://localhost:3000';
-let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJsdWNhcyIsImxhc3ROYW1lIjoiUml2YWRlbmVpcmEiLCJlbWFpbCI6ImxybGljaGFyZGlAaG90bWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCQ5cEsuekdLcjFNMGJKNXo5U3hxcVJla0NDSVIxdHBLVmlkQU9FQnJMRjFLd0NoSlZnalJzMiIsInJvbGVJZCI6MiwiaWF0IjoxNjQ4NzY3MzA5LCJleHAiOjE2NDg4NTM3MDl9.EOXM4vxoRUGVgtMHV2fzIVMWHOkw3NKxeH9GOotYk3c';
+const tokenInvalid = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJsdWNhcyIsImxhc3ROYW1lIjoiUml2YWRlbmVpcmEiLCJlbWFpbCI6ImxybGljaGFyZGlAaG90bWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCQ5cEsuekdLcjFNMGJKNXo5U3hxcVJla0NDSVIxdHBLVmlkQU9FQnJMRjFLd0NoSlZnalJzMiIsInJvbGVJZCI6MiwiaWF0IjoxNjQ4NzY3MzA5LCJleHAiOjE2NDg4NTM3MDl9.EOXM4vxoRUGVgtMHV2fzIVMWHOkw3NKxeH9GOotYk3c';
+const tokenValid = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJsdWNhcyIsImxhc3ROYW1lIjoiUml2YWRlbmVpcmEiLCJlbWFpbCI6ImxybGljaGFyZGlAaG90bWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCQ5cEsuekdLcjFNMGJKNXo5U3hxcVJla0NDSVIxdHBLVmlkQU9FQnJMRjFLd0NoSlZnalJzMiIsInJvbGVJZCI6MiwiaWF0IjoxNjQ4NzY3MzA5LCJleHAiOjE2NDg4NTM3MDl9.vxoRUGVgtMHV2fzIVMWHOkw3NKxeH9GOotYk3c'
 
 describe(
   'Api /authentication',
   () => {
-    describe('Get data user specific', () => {
-      it('Should respond with json containing data user specific', (done) => {
+    it('Should respond with json containing data user specific', (done) => {
+      chai
+        .request(url)
+        .get('/auth/me')
+        .set({ Authorization: tokenValid })
+        .end((err, res) => { 
+          expect(res).to.have.status(OK);
+          done();
+        });
+    });
+      it('Should respond with error for token invalid', (done) => {
         chai
           .request(url)
           .get('/auth/me')
-          .set({ Authorization: token })
-          .end((err, res) => {
-            expect(res).to.have.status(OK);
+          .set({ Authorization: tokenInvalid })
+          .end((err, res) => { 
+            expect(res).to.have.status(UNAUTHORIZED);
             done();
           });
       });
-    });
-  },
-  () => {
-    describe('Error at get data user specific for token', () => {
       it('Should respond with json containing error for token', (done) => {
         chai
           .request(url)
           .get('/auth/me')
           .set({ Authorization: '' })
           .end((err, res) => {
-            expect(err).to.have.status(FORBIDDEN);
+            expect(res).to.have.status(FORBIDDEN);
             done();
           });
       });
-    });
-  }
-);
+  });
+  // 
